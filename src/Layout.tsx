779ed5685fcf8794545439fc2a90f0b8c42f9b14
Alexey <alexey@stakhanovite.io>
2019-11-19 15:48:58 +0100

    first commit

---
+++ src/Layout.tsx
@@ -0,0 +1,30 @@
+//import BusyIndicator from 'react-busy-indicator@1.0.0'
+import React from 'react'
+import { NotFoundBoundary } from 'react-navi'
+import { Footer, Menu } from './Components';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+export default function Layout({ children }) {
+  return (
+    <div className="Layout" >
+      <CssBaseline />
+      <header className="Layout-header">
+        <Menu></Menu>
+      </header>
+      <main>
+        <NotFoundBoundary render={renderNotFound}>
+            {children}
+            <Footer></Footer>
+        </NotFoundBoundary>
+      </main>
+    </div>
+  )
+}
+
+function renderNotFound() {
+  return (
+    <div className='Layout-error'>
+      <h1>404 - Not Found</h1>
+    </div>
+  )
+}
