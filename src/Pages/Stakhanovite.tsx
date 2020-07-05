3fa12220e706ce5655febc19b72635a527282bec
Malick Mbengue <46930715+psychomb@users.noreply.github.com>
2020-07-05 21:24:23 +0200

    Update Stakhanovite.tsx

--- src/Pages/Stakhanovite.tsx
+++ src/Pages/Stakhanovite.tsx
@@ -12,22 +12,11 @@ export function Stakhanovite() {
     <Page title="STAKHANOVITE STAKE POOL" subtitle="A Cardano stake pool. For the community - By the community" logo={logo}>
         <div>
             <h3>
-            USING <Link color="secondary" href="https://staking.cardano.org/en/delegation/">DAEDALUS</Link> OR <Link color="secondary" href="https://yoroi-wallet.com/">YOROI</Link> WALLETS, JOIN OUR POOL ON THE CARDANO INCENTIVIZED TESTNET.
+            USING <Link color="secondary" href="https://daedaluswallet.io/">DAEDALUS</Link> OR <Link color="secondary" href="https://yoroi-wallet.com/">YOROI</Link> WALLETS, JOIN OUR POOL ON THE CARDANO MAINNET (Soon TM).
             </h3>
             <h1>
             WE ARE STKH!
             </h1>
-            <div style={{overflowWrap: "break-word"}}>
-                <span style={{color: "#e83e8c"}}>Pool ID: <a href="https://shelleyexplorer.cardano.org/en/stake-pool/3a6c4c5af3454634a5de5899554d219878efd609c73b5443b2f5b1a677f9a2a9/">3a6c4c5af3454634a5de5899554d219878efd609c73b5443b2f5b1a677f9a2a9</a></span>
-                <IconButton
-                    style={{width: "80px"}}
-                    color="inherit"
-                    title="Copy pool address"
-                    onClick={() => clipboard.copy('3a6c4c5af3454634a5de5899554d219878efd609c73b5443b2f5b1a677f9a2a9')}
-                >
-                    <FileCopyIcon />
-                </IconButton>
-            </div>
         </div>
     </Page>);
 }
