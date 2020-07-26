c4a27d501a8bff0fcb7a0f23d88275972465580d
Malick Mbengue <46930715+psychomb@users.noreply.github.com>
2020-07-26 23:23:45 +0200

    Update What.tsx (#93)

--- src/Pages/What.tsx
+++ src/Pages/What.tsx
@@ -13,13 +13,13 @@ export function What() {
                 Cardano, the blockchain supporting the ada cryptocurrency (ADA), runs in a <strong>proof-of-stake (PoS)</strong> setting. Anyone who owns ada can participate in its functioning. For that however, you must be online at all times…
                 <br />
                 <br />
-                Because this last requirement is very demanding and not practical for everyday users, Cardano allows you to delegate your ada (or stake) to a stake pool. 
+                Because this last requirement is very demanding and not practical for everyday users, you can delegate your ada (or stake) to a stake pool. 
                 <br />
                 <br />
-                The stake pool <strong>will be online for you</strong> and perform block validation on your behalf.
+                The stake pool <strong>will be online for you</strong> and perform blocks validation on your behalf.
                 <br />
                 <br />
-                <strong>That is exactly what the Stakhanovite Stake Pool does for you!</strong>
+                <strong>That is exactly what the Stakhanovite Stake Pool does for you: sign blocks 24/7/365.</strong>
             </div>
         </div>
     </Page>);
