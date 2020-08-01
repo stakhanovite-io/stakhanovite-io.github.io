70087164555b13d751d92c8afbbf8de1130c1797
alexey1935 <alexey@stakhanovite.io>
2020-08-02 00:07:12 +0200

    The copy

--- src/Pages/Stakhanovite.tsx
+++ src/Pages/Stakhanovite.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import Link from '@material-ui/core/Link';
 import IconButton from '@material-ui/core/IconButton';
+import Link from '@material-ui/core/Link';
 import FileCopyIcon from '@material-ui/icons/FileCopy';
 import { useClipboard } from 'use-clipboard-copy';
 import { Page } from './../Page';
@@ -11,18 +11,31 @@ export function Stakhanovite() {
     return (
     <Page title="STAKHANOVITE STAKE POOL" subtitle="A Cardano stake pool. For the community - By the community" logo={logo}>
         <div>
-            <h3>
-            USING <Link color="secondary" href="https://daedaluswallet.io/">DAEDALUS</Link> OR <Link color="secondary" href="https://yoroi-wallet.com/">YOROI</Link> WALLETS, JOIN OUR POOL ON THE CARDANO MAINNET FOR A FEE OF ONLY 1.935%.
-            </h3>
             <h1>
             WE ARE STKH, a minimum fixed cost and 1.9% margin fee Staking Pool.
-            NAME: Stakhanovite #1    
-            TICKER: STKH1
             </h1>
             <h2>
-            POOL ID:
-            b62ecc8ce7e46c4443b63b91fffaeb19f869d191a7d2381087aaa768      
+            NAME:
+            Stakhanovite #1
             </h2>
+            <h2>
+            TICKER:
+            STKH1
+            </h2>
+            <div>
+                <h2 style={{display: "inline"}}>Pool ID: <a href="https://pooltool.io/pool/b62ecc8ce7e46c4443b63b91fffaeb19f869d191a7d2381087aaa768/blocks">b62ecc8ce7e46c4443b63b91fffaeb19f869d191a7d2381087aaa768</a></h2>
+                <IconButton
+                    style={{width: "80px"}}
+                    color="inherit"
+                    title="Copy pool address"
+                    onClick={() => clipboard.copy('b62ecc8ce7e46c4443b63b91fffaeb19f869d191a7d2381087aaa768')}
+                >
+                    <FileCopyIcon />
+                </IconButton>
+            </div>
+            <h3>
+            JOIN OUR POOL ON THE CARDANO MAINNET USING <Link color="secondary" href="https://daedaluswallet.io/">DAEDALUS</Link> OR <Link color="secondary" href="https://yoroi-wallet.com/">YOROI</Link> WALLETS.
+            </h3>
         </div>
     </Page>);
 }
